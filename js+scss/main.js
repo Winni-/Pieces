f0036c1243f043b96dd6426aed715662ef4b6fc4
Winni <winni_ne@mail.ru>
2014-03-19 16:40:12 +0400

    Added custom select

--- js+scss/main.js
+++ js+scss/main.js
@@ -25,3 +25,16 @@ $("input:radio").click(function() {
     $("input:radio[name="+ name +"]").parent(".radio").removeClass("checked");
     $(this).parent(".radio").addClass("checked")
 })
+
+//select
+$("select").not(".select select").wrap(function() {
+    var claz ="";
+    if ($(this).attr("class")) {
+        claz = $(this).attr("class")
+    };
+    return "<div class='select "+ claz +"'>";
+}).each(function(ind, el) {
+    var select = $(el).outerWidth();
+    console.log(select)    
+    $(el).outerWidth(select+15).parent().outerWidth(select);
+})
